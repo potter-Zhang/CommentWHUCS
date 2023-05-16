c3bcb1b68a1f8ddd771aef85cea747449df80b73
JeromeYJ <3064325896@qq.com>
2023-05-16 23:13:15 +0800

    这里放一条备注

---
+++ CommentWHUCS/CommentWHUCS/Models/CompGroup.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CommentWHUCS.Models
+{
+    public class CompGroup
+    {
+        [Key]
+        public string GroupId { get; set; }
+        public string GroupName { get; set; }
+        public int OwnerId { get; set; }
+        public string Title { get; set; }
+        public string Text { get; set; }
+    }
+}
---
+++ CommentWHUCS/CommentWHUCS/Models/PartComp.cs
@@ -0,0 +1,9 @@
+﻿namespace CommentWHUCS.Models
+{
+    public class PartComp
+    {
+        public string Id { get; set; }
+        public string GroupId { get; set; }
+        public string TypeId { get; set; }
+    }
+}
