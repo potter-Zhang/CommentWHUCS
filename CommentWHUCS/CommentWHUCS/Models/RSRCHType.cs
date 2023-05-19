9e961cfd0ea5dd81fe03f8b14064629fac00a110
JeromeYJ <3064325896@qq.com>
2023-05-19 10:59:03 +0800

    删除了原来版本的CommentWHUCS

--- CommentWHUCS/CommentWHUCS/Models/RSRCHType.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CommentWHUCS.Models
-{
-    public class RSRCHType
-    {
-        public string Id { get; set; }
-        public string GeneralDir { get; set; }   //科研大方向
-        public string DetailedDir { get; set; }  //科研小方向
-    }
-}
