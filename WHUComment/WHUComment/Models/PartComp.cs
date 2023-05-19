c7f2a6224d8e12deb3c0270809c1850303a20389
@little__prince <13510222833@139.com>
2023-05-19 16:59:22 +0800

    update

--- WHUComment/WHUComment/Models/PartComp.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CommentWHUCS.Models
-{
-    public class PartComp
-    {
-        public string Id { get; set; }
-        public string GroupId { get; set; }
-        public string TypeId { get; set; }
-    }
-}
